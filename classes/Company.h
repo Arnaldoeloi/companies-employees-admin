0a383f839c979a7d703d6e5824d037165099a5a6
arnaldosouza <arnaldoeloi2@gmail.com>
2019-03-23 08:08:05 -0300

    Added methods to head classes.

--- classes/Company.h
+++ classes/Company.h
@@ -1,25 +1,33 @@
 #ifndef _COMPANY_H_
 #define _COMPANY_H_
 
+#include<iostream>
 #include<string>
 #include<Employee.h>
 #include<list>
 
 class Company{
     private:
-        string name;
-        string CNPJ;
-        list <Employee> employees;
+        string name_;
+        string CNPJ_;
+        list <Employee> employees_;
     public:
-        Company();
+        Company(string name, string CNPJ);
         ~Company();
 
-        void employ();
-        void fire();
-        void raiseEveryoneSalaryBy();
+        void employ(Employee e);
+        void fire(Employee e);
+        void raiseEveryoneSalaryBy(float percentage);
         void listNewPersonnel();
+        void countPersonnel();
+
         static void listEmployeeAveragePerCompany();
-        
+        static int N_COMPANIES;
+
+        friend ostream& operator<< (ostream &strteam, Company& const company);
+
+
+       
 
 };
 
--- classes/Employee.h
+++ classes/Employee.h
@@ -7,10 +7,26 @@
 class Employee{
 
     private: 
-        string name;
-        float salary;
-        Date admissionDate;
+        string name_;
+        float salary_;
+        Date admissionDate_;
+
+    public:
+        Employee(string name, float salary, Date admissionDate);
+        Employee(const Employee& e);
+
+        ~Employee();
+
+        void getName();
+        void setName(string name);
+
+        void getSalary();
+        void setSalary(float salary);
         
+        friend ostream& operator<< (ostream &strteam, Employee& const employee);
+        friend ostream& operator== (ostream &strteam, Employee& const employee);
+
+        static int N_EMPLOYEES;
 };
 
 
