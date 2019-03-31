659301f7a5a2bd24ae3b58bbea20821df1afd2cb
arnaldosouza <arnaldoeloi2@gmail.com>
2019-03-31 10:24:29 -0300

    Added comments on headers.

--- classes/Employee.h
+++ classes/Employee.h
@@ -16,23 +16,40 @@ class Employee{
 
     public:
 
+        //Constructor passing a date as argument
         Employee(std::string name, float salary, Date& admissionDate);
+        
+        //Constructor passing a string in date format (dd/mm/YYYY) as argument
         Employee(std::string name, float salary, std::string admissionDate);
         
+        //Copy constructor
         Employee(const Employee& e);
 
+        //Deconstructor
         ~Employee();
 
+        //Get name from Employee
         std::string getName();
+
+        //Set name for Employee
         void setName(std::string name);
 
+        //Get salary from Employee
         float getSalary();
+
+        //Get admission date from Employee
         Date getAdmissionDate();
         
+        //Set admission date for Employee
         void setAdmissionDate(Date& date);
+
+        //Set salary for Employee
         void setSalary(float salary);
 
+        //Cout operator overload
         friend std::ostream & operator << (std::ostream &stream, Employee& employee);
+        
+        //Equals to employee operator overload
         bool operator == (Employee& employee);
 
 };
--- main.cpp
+++ main.cpp
@@ -7,6 +7,10 @@
 
 int main(){
 
+
+    //Testing methods
+
+    //Creating a company and some Employees
     Company c1("GitBulb", "123478154");
     Date d1(10,12,2017);
     Employee e1("Arnaldo Barbosa Eloi de Souza", 1050.00, d1);
@@ -18,11 +22,13 @@ int main(){
     Employee e3("Senhor Miagui", 600.00, d3);
     std::cout<<"Employees: "<<Employee::N_EMPLOYEES<<std::endl;
     
+    //Employing people on company GitBulb
     c1.employ(e1);
     c1.employ(e2);
     c1.employ(e3);
     
 
+    //Creating a company and some Employees
     Company c2("Tindal Ships", "589124223");
     Date d4(10,12,2017);
     Employee e4("Magnaldo Cicilius da Costa", 10550.00, d4);
@@ -34,27 +40,39 @@ int main(){
     Employee e6("Renan Soares da Silva", 60000.00, d6);
     std::cout<<"Employees: "<<Employee::N_EMPLOYEES<<std::endl;
    
+    //Employing people on company Tindal Ships
     c2.employ(e4);
     c2.employ(e5);
     c2.employ(e6);
 
-    //Trying to employ someone previously hired
+    //Trying to employ someone previously hired, should alert an error with cout
     c2.employ(e6);
 
 
 
+    //Getting number of employees on GitBulb company
     std::cout<<c1.getNumberOfEmployees()<<std::endl;
+
+    //Getting static number of Companies and Employees
     std::cout<<"Companies: "<<Company::N_COMPANIES<<std::endl;
     std::cout<<"Employees: "<<Employee::N_EMPLOYEES<<std::endl;
     
+    //Getting number of employees on GitBulb company
     std::cout<<c2.getNumberOfEmployees()<<std::endl;
+
+    //Getting static number of Companies and Employees
     std::cout<<"Companies: "<<Company::N_COMPANIES<<std::endl;
     std::cout<<"Employees: "<<Employee::N_EMPLOYEES<<std::endl;
 
+    //Show average of employees per company
     std::cout<<"Average employee per company: "<<Company::employeePerCompanyAvg()<<std::endl; 
+    
+    //Testing cout overload from GitBulb and Tindal Ships
     std::cout<<c1<<std::endl; 
     std::cout<<c2<<std::endl; 
 
+    //Showing only people hired in the last 90 days
     c1.listNewPersonnel();
+    
     return 0;
 }
